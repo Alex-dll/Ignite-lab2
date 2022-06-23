3d6bdb481c8348816b33afe8c0aaeff8e82eb67f
alex-dll <alexsandrojr.303@gmail.com>
2022-06-22 23:14:40 -0300

    feat: :sparkles: make a new components to app

---
+++ src/components/Video.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Video: React.FC = () => {
+  return <h1 className="flex-1">Video</h1>;
+};
+
+export { Video };
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { ApolloProvider } from "@apollo/client";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+
 import { client } from "./lib/apolo";
 
 import "./styles/global.css";
